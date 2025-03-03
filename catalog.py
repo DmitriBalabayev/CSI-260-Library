37083ef0c4ce5104b1aa450b8474738ea5d1f97d
DmitriBalabayev <dmitri.balabayev@mymail.champlain.edu>
2025-03-03 14:43:57 -0500

    Update catalog.py

---
+++ catalog.py
@@ -0,0 +1,13 @@
+class Catalog(name,items):
+    def __init__ (self,name,items):
+        self.name = name
+        self.items = items
+
+    def add_library_item ():
+        pass
+    
+    def remove_library_item ():
+        pass
+    
+    def search ():
+        pass
